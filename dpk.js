a6ba57e1e9d967c02f84f916b0c118ed7b70d2bc
albert <albert.abril+macbook@gmail.com>
2023-05-16 18:43:03 +0200

    Refactor dpk module

--- dpk.js
+++ dpk.js
@@ -1,21 +1,42 @@
 const crypto = require("crypto");
 
-exports.deterministicPartitionKey = (event) => {
-  const TRIVIAL_PARTITION_KEY = "0";
-  const MAX_PARTITION_KEY_LENGTH = 256;
-  let candidate;
+const TRIVIAL_PARTITION_KEY = "0";
+const MAX_PARTITION_KEY_LENGTH = 256;
 
-  if (event && event.partitionKey) candidate = event.partitionKey;
-  if (event && !event.partitionKey) {
-    const data = JSON.stringify(event);
-    candidate = crypto.createHash("sha3-512").update(data).digest("hex");
+function getHashKey(data){
+  return crypto.createHash("sha3-512").update(data).digest("hex");
+}
+
+function getValidKey(currentKey){
+  if(!currentKey) return getHashKey(TRIVIAL_PARTITION_KEY);
+
+  let key = currentKey;
+
+  if(typeof currentKey !== "string") {
+    key = JSON.stringify(currentKey);
   }
 
-  if(candidate && typeof candidate !== "string" ) candidate = JSON.stringify(candidate);
-  if(!candidate) candidate = TRIVIAL_PARTITION_KEY;
-
-  if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-    candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
+  if (currentKey.length > MAX_PARTITION_KEY_LENGTH) {
+     key = getHashKey(currentKey);
   }
-  return candidate;
+  return key;
+}
+
+function deterministicPartitionKey(event) {
+
+  if(typeof event === "string"){
+    return getValidKey(event);
+  }
+
+  if(event && event.partitionKey) {
+    return getValidKey(event.partitionKey);
+  }
+  
+  return TRIVIAL_PARTITION_KEY;
 };
+
+module.exports = {
+  TRIVIAL_PARTITION_KEY,
+  MAX_PARTITION_KEY_LENGTH,
+  deterministicPartitionKey
+}
