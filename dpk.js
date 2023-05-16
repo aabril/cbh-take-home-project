a7434a9d0c9f3e4fc7a585475b5e76a917089705
albert <albert.abril+macbook@gmail.com>
2023-05-16 17:29:05 +0200

    Refactor: avoid nested conditionals

--- dpk.js
+++ dpk.js
@@ -5,22 +5,15 @@ exports.deterministicPartitionKey = (event) => {
   const MAX_PARTITION_KEY_LENGTH = 256;
   let candidate;
 
-  if (event) {
-    if (event.partitionKey) {
-      candidate = event.partitionKey;
-    } else {
-      const data = JSON.stringify(event);
-      candidate = crypto.createHash("sha3-512").update(data).digest("hex");
-    }
+  if (event && event.partitionKey) candidate = event.partitionKey;
+  if (event && !event.partitionKey) {
+    const data = JSON.stringify(event);
+    candidate = crypto.createHash("sha3-512").update(data).digest("hex");
   }
 
-  if (candidate) {
-    if (typeof candidate !== "string") {
-      candidate = JSON.stringify(candidate);
-    }
-  } else {
-    candidate = TRIVIAL_PARTITION_KEY;
-  }
+  if(candidate && typeof candidate !== "string" ) candidate = JSON.stringify(candidate);
+  if(!candidate) candidate = TRIVIAL_PARTITION_KEY;
+
   if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
     candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
   }
